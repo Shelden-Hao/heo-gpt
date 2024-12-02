5e9ae892862c90816b7a474ef78608d61be9f934
Heo Hao <2483386026@qq.com>
2024-12-02 13:37:43 +0800

    feat: 首页快速对话的实例

--- components/home/Main/index.tsx
+++ components/home/Main/index.tsx
@@ -3,7 +3,7 @@ import Welcome from "@/components/home/Main/Welcome";
 
 export default function Navigation() {
     return (
-        <nav className={`relative flex-1 bg-white text-gray-900 dark:bg-gray-800 dark:text-gray-100`}>
+        <nav className={`overflow-y-auto relative flex-1 bg-white text-gray-900 dark:bg-gray-800 dark:text-gray-100`}>
             <Menu/>
             <Welcome />
         </nav>
